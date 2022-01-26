8a8244e674e84a8c53c7a46afd63cf5f010c392f
Xueyan Shao <xueyanshaoshirley@gmail.com>
2022-01-25 20:59:54 -0800

    week4-day1-homework first commit

---
+++ js/randomcodes.js
@@ -0,0 +1,22 @@
+/* -----Random Codes -----*/
+//Function to generate combination of characters
+function generateCode() {
+//Create variables to store generated codes and the type of chracters we want to show as codes
+var code = ' '; //initiate the null value
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+//Generate character multiple times using a loop
+for (i=1;i<=8;i++) {
+    var char = Math.random()* str.length; //random select a character
+    code += str.charAt(char); //accumulate the generated character into a string of 8
+}
+
+return code; //return the final accumulated string when loop ends
+}
+//Get HTML element to display
+document.getElementById("codes").innerHTML = generateCode();
+//Disable Button
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+//Activate function
+disableButton();

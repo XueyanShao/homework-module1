7daa67a138aeacdfe07550e97e542b5b4f38b5b9
Xueyan Shao <xueyanshaoshirley@gmail.com>
2022-01-27 22:10:45 -0800

    week4-day2-homework first commit

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -2,7 +2,9 @@
 //Function to generate combination of characters
 function generateCode() {
 //Create variables to store generated codes and the type of chracters we want to show as codes
-var code = ' '; //initiate the null value
+var code = ' '; //to store generated codes and initiate to empty value
+var getCode = ' '; //to store entered code
+var btnvalue; //for button boolean value
 var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 //Generate character multiple times using a loop
 for (i=1;i<=8;i++) {
@@ -15,8 +17,30 @@ return code; //return the final accumulated string when loop ends
 //Get HTML element to display
 document.getElementById("codes").innerHTML = generateCode();
 //Disable Button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue; //able/disable button
+    if (btnvalue == true) { //test if button is diabled or enabled
+//set button and label color translucent
+    document.getElementById("submit").style.backgroundcolor = "rgba(73,119,209,0.3)";
+    document.getElementById("submit").style.color = "rgba(255,255,255,0.5)";
+    } else {
+        //set button and label color with no transparency
+        document.getElementById("submit").style.backgroundcolor = "rgba(73,119,209,1)";
+    document.getElementById("submit").style.color = "rgba(255,255,255,1)";
+    }
+    }
+    //listen to user input code
+    var codebox = document.getElementById("codeentered"); //get textbox
+    codebox.addEventListener("input", evaluateCode); //Listen to code entered in textbox
+    //run function if detected user had entered a character in textbox
+    function evaluateCode() {
+        getCode = document.getElementById("codeentered").value; //get character entered
+        var charset1 = getCode.trim(); //remove any hidden characters entered
+        var charset2 = code.trim(); //remove any hidden characters generated 
+        //test if code entered matches the number of generated characters
+        if (charset1.length == charset2.length && charset1 == charset2) {
+            disableButton(false); //if match, run the function to enable button
+        }
 }
 //Activate function
 disableButton();

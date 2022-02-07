7952cecdfd4ff18fb85ff74f49b476fcf43d5656
Xueyan Shao <xueyanshaoshirley@gmail.com>
2022-02-06 18:09:51 -0800

    week5-day3-homework first commit

--- js/activity.js
+++ js/activity.js
@@ -8,14 +8,34 @@ $(document).ready(function() {
 //     $(content).css("background-color","green");
 //     })
 
-$("td").hover(function() {
+$("td").click(function() {
     var content = $(this).text();
         if (content != "Not Available") {
-            $(this).css("cursor","pointer")
-            $(this).click(function() {
-                $(this).toggleClass("highlight")
+                $(this).css("cursor","pointer")
+                $(this).toggleClass("highlight");
+                
+
+            if($(this).hasClass("highlight")) {
+                $("#displaySelected").css({
+                    "visibility":"visible",
+                    "margin-top":"2em"
+                }) 
+                $("#result").append("<p>"+content+"</p>");
+            } 
+            else {
+
+                $("#result p:contains('" +content+ "')").remove();
+
+                }
+                    if($("#result").has("p").length==false) {
+                    $("#displaySelected").css({
+                         "visibility":"hidden",
+                        "margin-top":"0"
+                })
+             } 
+                 }
+             
             })
-}
-})
+
 })
 
